bd9b8cf3fb56914a7f8e62f5e11944693d3de63a
Ajeet Sharma <asharma@webcubator.co>
2020-01-15 13:30:22 +0530

    Second commit

---
+++ .github/secrets/decrypt_secrets.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/d6a4f1d0-701f-4049-98fe-1bb25a7c85d2.mobileprovision
+
+
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
